cf4da85df15f1baed7de413e45fe39af8c66d6ff
Bruno Lima <brunolimo125@gmail.com>
2024-07-16 14:14:30 -0300

    tests: refatorando o teste

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,23 @@
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from time import sleep
+import pytest
+import subprocess
 
-driver = webdriver.Chrome()
-
-driver.set_page_load_timeout(5)
-
-try:
-    driver.get("http://google.com")
-    sleep(5)
-    print("Acessou com sucesso")
-except TimeoutException:
-    print("Falha ao acessar")
-finally:
+#Código que será chamado para todos os testes
+@pytest.fixture
+def driver():
+    process = subprocess.Popen(['python','-m', 'streamlit','run','src/app.py'])
+    
+    driver = webdriver.Chrome()
+    driver.set_page_load_timeout(5)
+    yield driver
+    
     driver.quit()
+    process.kill()
+    
+def test_app_opens(driver):
+    driver.get("http://localhost:8501")
+    sleep(5)
+
+
